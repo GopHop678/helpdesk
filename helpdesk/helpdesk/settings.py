b2d5117432d41fb5048695867f0d88c61cbecec2
GopHop678 <ostudin02@mail.ru>
2025-10-22 16:20:17 +0800

    Add files via upload

--- helpdesk/helpdesk/settings.py
+++ helpdesk/helpdesk/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'main',
     'api',
 ]
@@ -49,6 +50,8 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -148,3 +151,26 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SITE_ID = 1
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+CORS_ALLOW_METHODS = [
+    'GET',
+    'PUT',
+]
--- helpdesk/static/scripts/request_details.js
+++ helpdesk/static/scripts/request_details.js
@@ -1,4 +1,69 @@
+function displayCategory(category) {
+    const categories = {
+        PRINTER: 'Принтер',
+        SOFTWARE: 'Программа',
+        HARDWARE: 'Компьютер',
+        ETC: 'Другое',
+    }
+    return categories[category];
+}
+
+
+function convertDateTime(inputDateTime) {
+    // Создаем объект Date из входной строки
+    const date = new Date(inputDateTime);
+    // Извлекаем компоненты даты
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = String(date.getFullYear()).slice(-2);
+    // Извлекаем компоненты времени
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    // Форматируем результат
+    return `${day}.${month}.${year} | ${hours}:${minutes}`;
+}
+
+
+
+async function showPopup (e) {
+    const requestId = e.id.slice(8);
+    const response = await fetch(`http://localhost:8000/api/request/${requestId}`);
+    const requestObj = await response.json();
+
+    if (!requestObj) {
+        return;
+    }
+
+    const popup = document.getElementById('request-details-popup');
+    popup.addEventListener('click', (e) => {
+        // Если клик был на самом popup (не на его дочерних элементах)
+        if (e.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+
+    const wrapper = popup.querySelector('.request-details-wrapper');
+    const requestHeader = wrapper.querySelector('.request-details-header');
+        const requestCategory = requestHeader.querySelector('.request-details-category');
+            requestCategory.textContent = displayCategory(requestObj.category);
+        const requestTime = requestHeader.querySelector('.request-details-time');
+            requestTime.textContent = convertDateTime(requestObj.request_date);
+        const requestSender = requestHeader.querySelector('.request-details-sender');
+            requestSender.textContent = requestObj.sender.full_name;
+        const requestPlace = requestHeader.querySelector('.request-details-place');
+            requestPlace.textContent = requestObj.place;
+    const requestBody = wrapper.querySelector('.request-details-body');
+        const requestText = requestBody.querySelector('.request-details-text');
+            requestText.textContent = requestObj.request_text;
+
+    popup.style.display = 'flex';
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
-    const popup = ''
+    const detailsButtons = Array.from(document.getElementsByClassName('request-details-btn'));
+    // console.log(detailsButtons);
+
+    detailsButtons.forEach(detailsButton =>
+        detailsButton.addEventListener('click', () => {showPopup(detailsButton)}))
 });
